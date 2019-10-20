9a08eae65a791c89c31dbb931fb8bc7bddd27e0d
Brian Wiborg <baccenfutter@c-base.org>
2019-10-20 13:27:07 +0200

    rearranging code; structureal improvement

--- log.go
+++ log.go
@@ -1,37 +1,9 @@
 package log
 
 import (
-	"os"
-
-	"github.com/octogo/log/pkg/config"
 	"github.com/octogo/log/pkg/level"
 	"github.com/octogo/log/pkg/log"
 )
-
-// ConfigFile defines the file-name of the configuration file.
-var ConfigFile = "logging.yml"
-
-// LoadConfig returns the loaded configuration.
-func LoadConfig(paths ...string) *config.Config {
-	if paths == nil || len(paths) == 0 {
-		paths = []string{ConfigFile}
-	}
-	return config.Load(paths...)
-}
-
-// Init initialized octolog.
-func Init() {
-	if _, err := os.Stat(ConfigFile); err != nil {
-		log.Init()
-	} else {
-		log.Configure(config.Load(ConfigFile))
-	}
-}
-
-// InitWithConfig intializes octolog with a custom configuration.
-func InitWithConfig(c *config.Config) {
-	log.Configure(c)
-}
 
 // New returns an initialized Logger with the given name.
 // If a Logger with the given name has already been registered, then that
